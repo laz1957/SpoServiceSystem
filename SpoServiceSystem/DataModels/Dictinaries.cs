3a906875161d0ec8adffde36e380f304bf8c5bb7
laz1957 <ulazarev@kait20.ru>
2024-09-26 16:58:28 +0300

    Добавьте файлы проекта.

---
+++ SpoServiceSystem/DataModels/Dictinaries.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SpoServiceSystem.DataModels
+{
+    public class TipUch : INotifyPropertyChanged
+    {
+        int id;
+        string name = "";
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public TipUch() {
+            id = 0; name = string.Empty;
+        }
+        public TipUch(int _id, string _name)
+        {
+            id = _id; name = _name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+
+    public class TipUchList:ObservableCollection<TipUch>
+    {
+        public TipUchList() { }
+
+    }
+}
---
+++ SpoServiceSystem/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpoServiceSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+          
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+            switch (e.Parameter.ToString())
+            {
+                case "BO":
+                      SpoServiceSystem.Windows.WindowTipUch win = new SpoServiceSystem.Windows.WindowTipUch();
+                      win.Show();
+                    break;
+                case "UO":
+                    
+                    break;
+
+            }
+
+
+
+
+            
+        }
+    }
+}
